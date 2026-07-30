683d73ddc4e1077f548b089a400e22ec44ff06fa
Fin <anoun@mailbox.org>
2026-07-30 16:52:33 +0300

    Current stable version - improved logging test v0.2

--- cmd/server/functionality/handlers.go
+++ cmd/server/functionality/handlers.go
@@ -295,7 +295,7 @@ func (a *ApiConfig) UpdateAndRestart(w http.ResponseWriter, r *http.Request) {
 
 func runUpdateDetached() error {
 	h, m, s := time.Now().Clock()
-	timeStamp := fmt.Sprintf("nohup ./localupdate.sh > logs/%d-%d-%d-update.log 2>&1 &", h, m, s)
+	timeStamp := fmt.Sprintf("nohup ./localupdate.sh > /logs/%d-%d-%d-update.log 2>&1 &", h, m, s)
 	cmd := exec.Command("bash", "-c", timeStamp)
 	//cmd := exec.Command("bash", "-c", "nohup ./localupdate.sh > update.log 2>&1 &")
 	cmd.Dir = "."
